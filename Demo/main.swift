621566c14f3f23e60fadb36b8bb01de2a774dab2
MacStudent <macstudent@126-imac16.local>
2019-02-08 18:58:59 -0500

    StringHandling samples

--- Demo/main.swift
+++ Demo/main.swift
@@ -10,3 +10,40 @@ import Foundation
 
 print("Hello, World!")
 
+var s: String = "Hello"
+print(s)
+
+
+var str = String()
+str = "Hello World"
+var p : String
+p = "Call me"
+print(p, str, s)
+print(str.description)
+
+print(str.hasPrefix("Hello"))
+print(s.hasPrefix("Hello"))
+print(s.count)
+
+print(s.uppercased())
+
+//print(p.index(after: ))
+
+//Display Characters from String
+
+for c in str{
+    print(c)
+}
+
+
+var x = 5...10
+ for i in x
+
+{
+    print(i)
+}
+ print(x)
+
+var y = 1..<10
+
+print(y)
