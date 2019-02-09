f49f9917c340cecbfd0cb1e92608d20e28f2f8ae
MacStudent <macstudent@126-imac16.local>
2019-02-08 20:54:59 -0500

    Set and Dictionary Functions

--- ArrayTest/ArrayTest/main.swift
+++ ArrayTest/ArrayTest/main.swift
@@ -28,3 +28,56 @@ for i in 0..<a.count
 //var b = [(a[1]*a[2]*a[3]*a[4]), (a[2]*a[3]*a[4]), (a[1]*a[3]*a[4]), (a[3]*a[4]) ]
 print(a)
 print(b)
+
+//-- SET Example
+print("__________________")
+
+var s1 = Set<String>()
+s1.insert("Canada")
+s1.insert("India")
+s1.insert("Russia")
+
+print(s1)
+print("__________________________________")
+print("Dictionary Example")
+var d = [1: "Hello",
+         2: "Value"]
+print(d)
+
+d[3]  = "Hello World"
+
+print(d[4])
+print("__________________________")
+
+var countary = Dictionary<String, String>()
+countary.updateValue("INDIA", forKey:"IND")
+countary.updateValue("Russia", forKey:"Rus")
+countary.updateValue("Pakistan", forKey:"Pak")
+countary.updateValue("Canada", forKey:"CAD")
+countary.updateValue("Brazil", forKey:"BZL")
+
+print(countary)
+print("__________________________")
+for i in countary
+{
+    print(i)
+    print(i.key, i.value)
+}
+print("__________________________")
+for (k,v) in countary
+{
+    print(k,v)
+}
+
+print("__________________________")
+for(_, v) in countary
+{
+    print(v)
+}
+
+print("__________________________")
+
+for k in countary.keys
+{
+    print(k)
+}
