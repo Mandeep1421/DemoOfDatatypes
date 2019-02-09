0e3e66f2e369d9f5a3da9bcb6272f481206bd35b
MacStudent <macstudent@126-imac16.local>
2019-02-08 20:09:25 -0500

    array handling

--- Demo/main.swift
+++ Demo/main.swift
@@ -58,3 +58,50 @@ if (z.contains(52))
 else{
     print("False")
 }
+
+
+
+
+
+
+
+
+
+
+print("_________________________________")
+
+var a = [10,20,40,50]
+print(a)
+print(a.count)
+for b in a
+{
+    print(b)
+}
+print(a[0], a[1], a[2], a[3])
+
+//a[5] = 1000
+//print(a[5])
+
+var c = [Int]()
+c.append(100)
+c.append(200)
+c.append(300)
+print(c[0], c[1], c[2])
+
+print("Value of B array")
+
+let B = a + c
+
+for  i in B
+{
+    print(i)
+}
+
+print( " Value of C[1..3] array")
+
+
+
+
+
+
+
